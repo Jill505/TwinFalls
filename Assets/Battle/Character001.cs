51984da73f0a56bc0f1cc71a286be6140c6e67db
adam1017 <wangadam1017@gmail.com>
2023-04-16 20:26:45 +0800

    好也

--- Assets/Battle/Character001.cs
+++ Assets/Battle/Character001.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character001 : MonoBehaviour
+public class Character001 : CharacterBase
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Battle/CharacterBase.cs
+++ Assets/Battle/CharacterBase.cs
@@ -8,14 +8,17 @@ public class CharacterBase:MonoBehaviour
     public Rigidbody2D rb;
     public Collider2D col;
     public Camera camera;
+    Transform myTF;
 
     public float moveSpeed;
+    public float jumpForce = 10f;
 
     public void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         col = gameObject.GetComponent<Collider2D>();
         camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        myTF = gameObject.GetComponent<Transform>();
     }
 
     public virtual void fire()
@@ -25,7 +28,7 @@ public class CharacterBase:MonoBehaviour
 
     public virtual void usingSkill()
     {
-
+        
     }
 
     public virtual void kill()
@@ -43,6 +46,7 @@ public class CharacterBase:MonoBehaviour
         {
             rb.velocity = new Vector2(-1 * moveSpeed, rb.velocity.y);
         }
+        /*
         if (Input.GetKey(KeyCode.S))
         {
             rb.velocity = new Vector2(rb.velocity.x, -1 * moveSpeed);
@@ -50,22 +54,44 @@ public class CharacterBase:MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             rb.velocity = new Vector2(rb.velocity.x, 1 * moveSpeed);
-        }
-
+        }*/
+        /*
         if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
             rb.velocity = new Vector2(rb.velocity.x, 0f);
-        }
+        }*/
         if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(0f,rb.velocity.y);
         }
 
+        jump();
+
         //facing
+        /*
         float diraction;
         Vector2 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        Vector2 calXY = (Vector2)transform.position - mousePos;
-        diraction = Mathf.Atan2(calXY.y, calXY.x) * 180f/Mathf.PI;
-        transform.localEulerAngles = new Vector3(0f, 0f, diraction);
+        Debug.Log("mouse pos is" + mousePos);
+        Vector2 calXY = ((Vector2)transform.position - mousePos).normalized;
+        diraction = Mathf.Atan2(calXY.y, calXY.x) * Mathf.Rad2Deg;
+        Debug.Log("the dir is:" + diraction);
+        transform.localEulerAngles = new Vector3(0f, 0f, diraction);*/
+        Vector2 mouseDirection;
+        Vector2 mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 AimDirection = mousePosition - rb.position;
+        float AimAngle = Mathf.Atan2(AimDirection.y, AimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = AimAngle;
+        
+    }
+
+    public void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Jump
+            rb.velocity += new Vector2(0f,1f*jumpForce);
+        }
     }
 }
